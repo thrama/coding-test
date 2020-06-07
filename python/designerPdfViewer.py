8bbfd9e4d18df04c7223ea3172c131adb458d07f
Lorenzo Lombardi <65155343+thrama@users.noreply.github.com>
2020-06-07 11:36:01 +0200

    fix the designerPdfViewer.py

--- python/designerPdfViewer.py
+++ python/designerPdfViewer.py
@@ -8,9 +8,11 @@ def designerPdfViewer(h, word):
     x = 0
 
     for i in range(len(word)):
-        if l.index(word[i]) > x:
+        if h[l.index(word[i])] > x:
             x = h[l.index(word[i])]
+            #print(f'h(i): {h[l.index(word[i])]}, x({i}) = {x}')
     
+    print(len(word))
     return x * len(word)
 
 
@@ -19,8 +21,11 @@ def designerPdfViewer(h, word):
 #word = 'abcz'
 
 # simple case 1
-h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7]
-word = 'zaba'
+#h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7]
+#word = 'zaba'
+
+h = [6, 5, 7, 3, 6, 7, 3, 4, 4, 2, 3, 7, 1, 3, 7, 4, 6, 1, 2, 4, 3, 3, 1, 1, 3, 5]
+word = 'zbkkfhwplj'
 
 result = designerPdfViewer(h, word)
 print(result)
