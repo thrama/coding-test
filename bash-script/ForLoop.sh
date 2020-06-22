dad111bec8b11db9f7f0ea521ba712c839c6ed2a
Lorenzo Lombardi <65155343+thrama@users.noreply.github.com>
2020-06-22 17:44:51 +0200

    update the file with comments

--- bash-script/ForLoop.sh
+++ bash-script/ForLoop.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+#
+# Your task is to use for loops to display only odd natural numbers from 1 to 99.
+#
+# Link: https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+#
+
 for i in {1..100}
 do
 	if [ $((i % 2)) != 0 ]; then
